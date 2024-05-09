bda8137250c66155b41e51e23189031585b545ef
MiguelQueiroz <miguelqueiroz360@gmail.com>
2024-05-09 11:29:18 -0300

    cpf check

--- DaRoça/DaRoca/cadastro.js
+++ DaRoça/DaRoca/cadastro.js
@@ -1,11 +1,52 @@
 function checkcpf(){
     let a = document.querySelector('#cpf').value;
-    for(let i = 11 - digits; i > 2; i -= 1)
-    {
-        
+    if(a.length != 11){
+        console.log("Tamanho Insuficiente")
+        return 2;
     }
+
+    let somatoria = 0;
+    let digito1 = 0;
+
+    for(let i = 0, j = 10; i < 9; i += 1, j -= 1)
+    {
+        somatoria += Number(a[i]) * j;
+    }
+
+    if(somatoria / 11 >= 2){
+        digito1 = 11 - (somatoria - (Math.floor(somatoria / 11) * 11));
+    }
+    else{
+        digito1 = 0;
+    }
+
+    if(!(Number(a[9]) == digito1)){
+        console.log("Falso");
+        return 0;
+    }
+
+    somatoria = 0;
+    let digito2 = 0;
+
+    for(let i = 0, j = 11; i < 10; i += 1, j -= 1)
+    {
+        somatoria += Number(a[i]) * j;
+    }
+
+    if(somatoria / 11 >= 2){
+        digito2 = 11 - (somatoria - (Math.floor(somatoria / 11) * 11));
+    }
+    else{
+        digito2 = 0;
+    }
+
+    if(!(Number(a[10]) == digito2)){
+        console.log("Falso");
+        return 0;
+    }
+
+    console.log("Verdade");
+    return 1;
 }
 
-function digits(){
-    let a = document.querySelector('#cpf').value;
-}
+checkcpf()
