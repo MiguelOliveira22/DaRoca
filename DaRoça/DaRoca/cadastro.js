6c02f746d9b42e9867f99d50adc1f50f30403640
MiguelQueiroz <miguelqueiroz360@gmail.com>
2024-05-09 10:10:53 -0300

    Update cadastro.js

--- DaRoça/DaRoca/cadastro.js
+++ DaRoça/DaRoca/cadastro.js
@@ -1,11 +1,11 @@
 function checkcpf(){
     let a = document.querySelector('#cpf').value;
-    for(let i = 11; i > 2; i -= 1)
+    for(let i = 11 - digits; i > 2; i -= 1)
     {
         
     }
 }
 
 function digits(){
-    
+    let a = document.querySelector('#cpf').value;
 }
