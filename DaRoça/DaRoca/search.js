66fceed1756525119c934bbcf2995de3695aacbe
MiguelQueiroz <miguelqueiroz360@gmail.com>
2024-05-26 19:24:29 -0300

    rarararar

--- DaRoça/DaRoca/search.js
+++ DaRoça/DaRoca/search.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function(){
     document.querySelector("#querymain").value = returner;
 
     let product = [
-        {"name": "Banana", "url": "banana-prata", "img": "./images/banana.png", "value": "R$2340.00", "unit": "per unit"},
+        {"name": "Banana", "url": "banana-prata", "img": "./images/banana.png", "categorias": "queijo teste doce", "value": "R$2340.00", "unit": "per unit"},
         {"name": "Banana", "url": "banana-prata", "img": "./images/banana.png", "value": "R$2340.00", "unit": "per unit"},
         {"name": "Banana", "url": "banana-prata", "img": "./images/banana.png", "value": "R$2340.00", "unit": "per unit"},
         {"name": "Banana", "url": "banana-prata", "img": "./images/banana.png", "value": "R$2340.00", "unit": "per unit"},
@@ -36,6 +36,7 @@ document.addEventListener("DOMContentLoaded", function(){
     let nProducts = product.length;
     let k = 0;
     let l = 0;
+    let isContent
     for(let j = 0; j < nProducts; j ++){
         if(k == 0){
             let creator = document.createElement("div");
@@ -43,10 +44,7 @@ document.addEventListener("DOMContentLoaded", function(){
             creator.className = "flex around rows";
             document.querySelector("#main-content").appendChild(creator);
             l ++;
-        }
-        k ++;
-        if(k == 6){
-            k = 0;
+            k ++;
         }
 
         if(product[j].name.includes(returner)){
@@ -87,7 +85,29 @@ document.addEventListener("DOMContentLoaded", function(){
             creator.className = "small";
             creator.innerHTML = product[j].unit;
             document.querySelector("#p" + j).appendChild(creator);
+            isContent = 1
+            k ++;
         }
 
+        if(k > 6){
+            k = 0;
+        }
+    }
+
+    if(!isContent){
+        document.querySelector("#div0").className = "flex centralize centralizenormal rows";
+
+        let creator = document.createElement("img");
+        creator.style.width = "350px";
+        creator.style.Height = "350px";
+        creator.src = "images/perdao.svg";
+        creator.className = "flex around rows";
+        document.querySelector("#div0").appendChild(creator);
+
+        creator = document.createElement("p");
+        creator.style.width = "350px";
+        creator.innerHTML = "Perdão, mas não conseguimos encontrar nenhum produto parecido com o descrito!";
+        creator.className = "flex around rows";
+        document.querySelector("#div0").appendChild(creator);
     }
 }, false)
