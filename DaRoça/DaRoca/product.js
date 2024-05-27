85987ce0e674991a6d526c9aade32da2aa1a5c04
MiguelQueiroz <miguelqueiroz360@gmail.com>
2024-05-27 00:27:00 -0300

    pagina carrega conteudo

--- DaRoça/DaRoca/product.js
+++ DaRoça/DaRoca/product.js
@@ -21,8 +21,46 @@ function call(){
                 inUse = product[i];
             }
         }
-        console.log(inUse)
+
+        let creator = document.createElement("img");
+        creator.src = inUse.img;
+        creator.id = "image-main"
+        document.querySelector("#image").appendChild(creator);
+
+        creator = document.createElement("h1");
+        creator.innerHTML = inUse.name;
+        creator.className = "nomargin";
+        document.querySelector("#name").appendChild(creator);
+
+        let inner = document.createElement("small");
+        inner.innerHTML = inUse.unit;
+        inner.className = "nomargin";
+
+        creator = document.createElement("p");
+        creator.id = "valuefinal";
+        creator.className = "nomargin paddown";
+        creator.innerHTML = inUse.value + " ";
+        document.querySelector("#valores").appendChild(creator);
+
+        document.querySelector("#valuefinal").appendChild(inner);
+
+        let quebra = inUse.categorias.split(" ");
+        for(let ji = 0; ji < quebra.length; ji ++){
+            creator = document.createElement("span");
+            creator.className = "nodeco newpad color background unbreakeable marginleft";
+            creator.innerHTML = quebra[ji];
+            document.querySelector("#categs").appendChild(creator);
+        }
+
+        creator = document.createElement("p");
+        creator.id = "valuefina";
+        creator.className = "nomargin";
+        creator.innerHTML = inUse.descriptor;
+        document.querySelector("#descript-container").appendChild(creator);
     });
+
+    // image-main
+    
 }
 
 document.addEventListener("DOMContentLoaded", call(), false)
