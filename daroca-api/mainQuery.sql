3df72d7de9fb0ca9bdb360c3f122be16361487a7
MiguelQueiroz <miguelqueiroz360@gmail.com>
2024-06-04 11:58:20 -0300

    rename + additions

---
+++ daroca-api/mainQuery.sql
@@ -0,0 +1,12 @@
+CREATE TABLE daroca.produtos(
+   id      INT  IDENTITY (1, 1) NOT NULL,
+   nome    VARCHAR (30) NOT NULL,
+   imagem  VARCHAR (50) NOT NULL,
+   valor   REAL NOT NULL,
+   unidade VARCHAR(15) NOT NULL,
+   descricao VARCHAR (50) NOT NULL,
+   categoria INT,
+   PRIMARY KEY CLUSTERED (id ASC)
+);
+
+insert into daroca.produtos values ('Miguel', 'images/alho.jfif', 2.99, 'per unit', 'muito bom e saboroso yummy', 1)
