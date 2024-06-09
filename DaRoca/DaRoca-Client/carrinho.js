9b15360cca5e6896d2ef1332e3592a30384cadf0
PedPietro <166723178+PedPietro@users.noreply.github.com>
2024-06-09 17:50:58 -0300

    Eureca
    puta merda era fácil enviar, só foi burro mesmo, o erro era porque algumas coisas estavam conflitando entre si

--- DaRoca/DaRoca-Client/carrinho.js
+++ DaRoca/DaRoca-Client/carrinho.js
@@ -3,28 +3,21 @@ const cart = document.getElementById('sidecart')
 const closeBtn = document.getElementById('close_btn')
 const backdrop = document.querySelector('.backdrop')
 
-openBtn.addEventListener('click',openCart)
-closeBtn.addEventListener('click',closeCart)
+openBtn.addEventListener('click',openCart) 
+closeBtn.addEventListener('click', closeCart)
 
 // Open Cart
 function openCart() {
-    cart.classList.add('open')
-    backdrop.style.display = 'block'
-    
-    setTimeout(() => {
-    backdrop.classList.add('show')
-    },0)
+  cart.classList.add('open')
+  backdrop.style.display = 'block'
+  backdrop.classList.add('show')
 }
 
 // Close Cart
 function closeCart() {
-    cart.classList.remove('open')
-    backdrop.classList.remove('show')
-
-    setTimeout(() => {
-     backdrop.style.display = 'nome'
-    },500)
-
-
-
+  cart.classList.remove('open')
+  backdrop.classList.remove('show')
+  setTimeout(() => {
+    backdrop.style.display = 'none'
+  }, 500)
 }
