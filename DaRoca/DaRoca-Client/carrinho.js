a4d68d293bb1e149c5d228113e6cbe2db5687929
MiguelQueiroz <miguelqueiroz360@gmail.com>
2024-06-09 18:39:15 -0300

    Revert "Eureca"
    This reverts commit 9b15360cca5e6896d2ef1332e3592a30384cadf0.

--- DaRoca/DaRoca-Client/carrinho.js
+++ DaRoca/DaRoca-Client/carrinho.js
@@ -3,21 +3,28 @@ const cart = document.getElementById('sidecart')
 const closeBtn = document.getElementById('close_btn')
 const backdrop = document.querySelector('.backdrop')
 
-openBtn.addEventListener('click',openCart) 
-closeBtn.addEventListener('click', closeCart)
+openBtn.addEventListener('click',openCart)
+closeBtn.addEventListener('click',closeCart)
 
 // Open Cart
 function openCart() {
-  cart.classList.add('open')
-  backdrop.style.display = 'block'
-  backdrop.classList.add('show')
+    cart.classList.add('open')
+    backdrop.style.display = 'block'
+    
+    setTimeout(() => {
+    backdrop.classList.add('show')
+    },0)
 }
 
 // Close Cart
 function closeCart() {
-  cart.classList.remove('open')
-  backdrop.classList.remove('show')
-  setTimeout(() => {
-    backdrop.style.display = 'none'
-  }, 500)
+    cart.classList.remove('open')
+    backdrop.classList.remove('show')
+
+    setTimeout(() => {
+     backdrop.style.display = 'nome'
+    },500)
+
+
+
 }
