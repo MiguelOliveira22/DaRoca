8d288a10060f555ef929a89616c158fdf6cf9d08
MiguelQueiroz <miguelqueiroz360@gmail.com>
2024-06-09 18:43:05 -0300

    Revert "aa"
    This reverts commit 720676911f7dc0ebc2eaa9102bbb631d6c79dc8b.

--- daroca-api/index.js
+++ daroca-api/index.js
@@ -12,10 +12,10 @@ app.use(cors());
 ///////////////////////////
 // SQL Server configuration
 var config = {
-    user: 'sa',
-    password: 'Ratinho0411',
-    server: 'localhost', // ou o endereço do servidor SQL Server
-    database: 'Teste',
+    user: 'BD24143',
+    password: 'Ratinho0411@',
+    server: 'regulus', // ou o endereço do servidor SQL Server
+    database: 'BD24143',
     options: {
         encrypt: false // Se você estiver usando uma conexão segura (HTTPS), defina como true
     }
